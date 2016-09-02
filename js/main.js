de52f8de4c7228350c964d7a60613cc3ba5f0c3f
Ryo Kato <ryo_kato@hashedhyphen.com>
2016-09-02 16:38:43 +0900

    Add a board

--- js/main.js
+++ js/main.js
@@ -2,9 +2,28 @@
 
 enchant();
 
+// inherit enchant.Map
+var Board = Class.create(enchant.Map, {
+  initialize: function() {
+    var core = Core.instance;
+
+    var squareSize = 30;
+    // `this` will be an instance of enchant.Map
+    enchant.Map.call(this, squareSize, squareSize);
+
+    this.x = 380;
+    this.y = 260;
+
+    this.image = core.assets["img/square.png"];
+    // 8x8 array filled with 0
+    this.loadData(Array(8).fill(Array(8).fill(0)));
+    this.collisionData = Array(8).fill(Array(8).fill(0));
+  }
+});
+
 // inherit enchant.Group
 var MacroBlock = Class.create(Group, {
-  initialize(x, y) {
+  initialize: function(x, y) {
     var core = Core.instance;
 
     // `this` will be an instance of enchant.Group
@@ -57,14 +76,23 @@ var MacroBlock = Class.create(Group, {
 
 window.onload = function() {
   var core = new Core(1000, 600);
-
   core.fps = 60;
-  core.preload("img/orangeBlock.png");
+  preloadAssets();
 
   core.addEventListener("load", function() {
+    var board = new Board();
+    core.rootScene.addChild(board);
+
     var mb = new MacroBlock(100, 100);
     core.rootScene.addChild(mb);
   });
 
   core.start();
 };
+
+function preloadAssets() {
+  var core = Core.instance;
+
+  core.preload("img/square.png");
+  core.preload("img/orangeBlock.png");
+}
